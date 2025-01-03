4cd2ac1eeab15e025c642c3834bf358fd7dc62a2
Kwang Eun Jang <kejang@stanford.edu>
2025-01-03 14:31:51 -0800

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="empty",
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,  # This line ensures data files are included
     install_requires=[],
